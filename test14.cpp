857b92be4275d10f02e5d8c8ca4c48cb873bfedc
harumo11 <s_harumo@hotmail.co.jp>
2017-12-15 17:09:08 +0900

    [add] added test program  test11, 13, 14.cpp

---
+++ test14.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "Pie.hpp"
+
+int main(int argc, char const* argv[])
+{
+	Pie plot;
+
+	plot.plot(30, "a");
+	plot.plot(70, "b");
+
+	plot.show();
+
+	plot.save_as_png("This_is_pie_tutorial");
+
+	std::cin.get();
+	return 0;
+}
