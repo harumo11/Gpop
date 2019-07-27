607a5bc4ff0fdd6897e6c9a1d41a5f51eb730372
harumo11 <s_harumo@hotmail.co.jp>
2019-07-27 18:20:46 +0900

    [update] test22.cpp, added namespace

--- test/test22.cpp
+++ test/test22.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
 #include <sstream>
 
-#include <gpop/Series.hpp>
+#include <Gpop/Series.hpp>
 
 int main(int argc, char const* argv[])
 {
 	std::vector<std::vector<double>> vec_table;
-	Series plot;
+	Gpop::Series plot;
 	for (int coeff = 1; coeff < 5; coeff++) {
 		std::vector<double> v;
 		for (int i = -100; i < 100; i++) {
